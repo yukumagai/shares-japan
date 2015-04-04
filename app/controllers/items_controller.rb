01bf63aba89e96005c6e4f2e96db91f4b49563ef
yukumagai <kumagaiyuu@kumagaiyuu-no-MacBook-Air.local>
2015-04-04 14:42:37 +0900

    eighty-commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,12 @@
 class ItemsController < ApplicationController
   before_filter :require_login
-  
+
   def index
     @user = current_user
   end
 
   def show
-    @user = User.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def edit
